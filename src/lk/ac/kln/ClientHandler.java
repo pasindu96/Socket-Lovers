d84344e230ab5da040c7615d97e1e58a99cffd20
pasindu.96 <pasindu.wijesinghe@gmail.com>
2020-06-21 23:03:12 +0530

    Completed the server part with client handler

--- src/lk/ac/kln/ClientHandler.java
+++ src/lk/ac/kln/ClientHandler.java
@@ -1,26 +1,52 @@
 package lk.ac.kln;
 
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientHandler extends Thread{
     private Socket client;
     private ChatServer server;
-    //private PrintWriter writer;
+    private PrintWriter writer;
 
     ClientHandler(Socket client,ChatServer server){
         this.client=client;
         this.server=server;
     }
-    public static void main(String[] args) {
-
-    }
-
     @Override
     public void run() {
-        //Client handling part
+        try {
+            InputStream input=client.getInputStream();
+            BufferedReader reader=new BufferedReader(new InputStreamReader(input));
+
+            OutputStream output=client.getOutputStream();
+            writer=new PrintWriter(output,true);
+
+            String username=reader.readLine();
+            server.addUserName(username);
+
+            String serverMessage="New User connected : "+username;
+            //Should broadcast to other uses when a new user connected to the server
+            server.broadcast(serverMessage,this);
+
+            String clientMessage;
+            do{
+                clientMessage=reader.readLine();
+                serverMessage = username + " : "+clientMessage;
+                server.broadcast(serverMessage,this);
+            }while(!clientMessage.equals("bye"));
+            //If  the user type bye in the chat user should remove from the server client list
+            server.removeUser(username,this);
+            client.close();
+
+            serverMessage= username + " has quitted..";
+            server.broadcast(serverMessage,this);
+
+        } catch (IOException e) {
+            System.out.println("Error in UserThread: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
     void sendMessage(String message){
-        //should use printwriter
+        writer.println(message);
     }
 }
