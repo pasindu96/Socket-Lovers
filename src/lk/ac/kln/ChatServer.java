0a321ae0a0acea7807776fe3afccb331dfbed5f2
pasindu.96 <pasindu.wijesinghe@gmail.com>
2020-06-21 11:31:00 +0530

    ChatServer class has been developed.Didn't complete the ClientHandler class

--- src/lk/ac/kln/ChatServer.java
+++ src/lk/ac/kln/ChatServer.java
@@ -1,8 +1,65 @@
 package lk.ac.kln;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ChatServer {
 
+    //Save the username of clients
+    private Set<String> userName=new HashSet<>();
+    //Save the client threads who are logging to the system
+    private Set<ClientHandler> clientThreads=new HashSet<>();
+
     public static void main(String[] args) {
-	// write your code here
+	    ChatServer server=new ChatServer();
+	    server.executeServer();
+    }
+
+    public void executeServer(){
+        try {
+            //Make a port for a server socket
+            ServerSocket socket=new ServerSocket(6000);
+            System.out.println("Created a new socket on port 6000");
+            while (true){
+                //Once a client is joined this will accept that client
+                Socket client=socket.accept();
+                System.out.println("A new user has connected ! ");
+                ClientHandler newUser=new ClientHandler(client,this);
+                clientThreads.add(newUser);
+                //ClientHandle extends from Thread class. So each user will assign for a separate thread and invoke start method
+                newUser.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //Get all the username of clients
+    public Set<String> getUserName() {
+        return userName;
+    }
+
+    //Add username for the thread
+    void addUserName(String name){
+        userName.add(name);
+    }
+
+    //Once a client send a message it should broadcast every client in the system
+    void broadcast(String message, ClientHandler excludeUser) {
+        for (ClientHandler aUser : clientThreads) {
+            if (aUser != excludeUser) {
+                aUser.sendMessage(message);
+            }
+        }
+    }
+    //Once the client exit from the system, this will remove the username of this user
+    void removeUser(String name, ClientHandler user) {
+        boolean removed = userName.remove(name);
+        if (removed) {
+            clientThreads.remove(user);
+            System.out.println("The user " + name + " quitted..");
+        }
     }
 }
--- src/lk/ac/kln/ClientHandler.java
+++ src/lk/ac/kln/ClientHandler.java
@@ -1,5 +1,26 @@
 package lk.ac.kln;
 
-public class ClientHandler {
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class ClientHandler extends Thread{
+    private Socket client;
+    private ChatServer server;
+    //private PrintWriter writer;
+
+    ClientHandler(Socket client,ChatServer server){
+        this.client=client;
+        this.server=server;
+    }
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void run() {
+        //Client handling part
+    }
+    void sendMessage(String message){
+        //should use printwriter
+    }
 }
